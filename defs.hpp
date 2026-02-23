3afa5f6e67c7de2d41492639a71bb0d6a0ce12d7
Enerzed <lasker7241@gmail.com>
2026-02-23 17:34:49 +0700

    add non terminals

--- defs.hpp
+++ defs.hpp
@@ -15,6 +15,8 @@
 typedef char TypeLex[MAX_LEX];
 typedef char TypeMod[MAX_TEXT];
 
+
+/** Tokens **/
 #define TIdent 1
 #define TConst10 11
 #define TConst16 12
@@ -49,3 +51,41 @@ typedef char TypeMod[MAX_TEXT];
 #define TLe 62
 #define TEnd 71
 #define TErr 72
+
+
+/** Nonterminals **/
+#define TPROGRAM 100
+#define TDESCRIPTORS 101
+#define TDESCRIPTOR 102
+#define TDATA 103
+#define TTYPE 104
+#define TLIST 105
+#define TLIST_END 106
+#define TELEMENT 107
+#define TELEMENT_END 108
+#define TARRAY_END 109
+#define TSIZE 110
+#define TARRAY_EXPRESSION 111
+#define TARRAY_LIST 112
+#define TARRAY_LIST_END 113
+#define TFUNCTION 114
+#define TCOMPOSITE_OPERATOR 115
+#define TOPERATORS_AND_DESCRIPTORS 116
+#define TOPERATORS_AND_DESCRIPTORS_END 117
+#define TBLOCK_ELEMENT 118
+#define TOPERATOR 119
+#define TEMPTYOPERATOR 120
+#define TCONDITION 121
+#define TCONDITION_END 122
+#define TOPERATOR_ID 123
+#define TOPERATOR_ID_END 124
+#define TEXPRESSION 125
+#define TEXPRESSION_END 126
+#define TCOMPARISON 127
+#define TCOMPARISON_END 128
+#define TADDENDUM 129
+#define TADDENDUM_END 130
+#define TMULTIPLICAND 131
+#define TMULTIPLICAND_END 132
+#define TUNARY 133
+#define TELEMENTARY 134
